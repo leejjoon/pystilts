33980bf9e47d17bb981cce6b2861c6c750f245ba
Jae-Joon Lee <lee.j.joon@gmail.com>
2021-05-28 00:59:30 +0900

    add initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='pystilts',
+      version='0.1',
+      description='galactic_extinction',
+      author='',
+      author_email='',
+      url='https://data.kasi.re.kr/gitlab/leejjoon/small-astronomy-modules',
+      packages=['pystilts'],
+      package_dir={'pystilts':'lib'}
+     )
+
